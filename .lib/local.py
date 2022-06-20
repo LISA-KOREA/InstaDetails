440d7d2c10361553b5aa9be9043f39cd50979e4e
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-06-21 02:07:53 +0530

    Create local.py

--- .lib/local.py
+++ .lib/local.py
@@ -62,7 +62,7 @@ def banner():
       ███████╗██║███████║██║  ██║                        │
       ╚══════╝╚═╝╚══════╝╚═╝  ╚═╝
         {gr}Code By :
-  {gr}LISA}
+  {gr}LISA{nu}
 	            """)
 
 useragent = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
